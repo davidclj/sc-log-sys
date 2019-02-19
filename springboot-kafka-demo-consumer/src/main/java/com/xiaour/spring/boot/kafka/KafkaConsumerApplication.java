928bd88315c6a6be1dacc5be2035382f42ac32e8
cuilijin <davidclj@gmail.com>
2019-02-19 13:27:48 +0800

    增加kafka操作

---
+++ springboot-kafka-demo-consumer/src/main/java/com/xiaour/spring/boot/kafka/KafkaConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.xiaour.spring.boot.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+}
